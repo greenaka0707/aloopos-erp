e0415f7cb7d8e68815b4a4ecdf18d70ace07991f
greenaka0707 <produkpebeka@gmail.com>
2026-05-26 17:35:19 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,16 @@
-window.onerror = function (msg, src, line, col, err) {
-  document.body.innerHTML = `
-    <pre style="
-      background:#000;
-      color:#00ff88;
-      min-height:100vh;
-      padding:20px;
-      white-space:pre-wrap;
-      font-size:14px;
-    ">
-${msg}
-
-${src}:${line}:${col}
-    </pre>
-  `;
-};
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <div
+    style={{
+      color: "white",
+      background: "black",
+      minHeight: "100vh",
+      padding: "40px",
+      fontSize: "32px",
+    }}
+  >
+    TEST BERHASIL
+  </div>,
 );
