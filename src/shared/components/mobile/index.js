55584de9d4c04fbb869e100bd2905b9ac62a3fe3
greenaka0707 <produkpebeka@gmail.com>
2026-05-27 20:08:03 +0700

    Create index.js

---
+++ src/shared/components/mobile/index.js
@@ -0,0 +1,8 @@
+export { default as MobilePage } from "./layout/MobilePage";
+export { default as MobileHeader } from "./layout/MobileHeader";
+export { default as MobileBottomNav } from "./layout/MobileBottomNav";
+
+export { default as MobileCard } from "./cards/MobileCard";
+
+export { default as MobileList } from "./lists/MobileList";
+export { default as MobileListItem } from "./lists/MobileListItem";
