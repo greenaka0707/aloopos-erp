f3e3ebc95971cb508cb89f12dbd6957738fcbe84
greenaka0707 <produkpebeka@gmail.com>
2026-05-29 14:56:50 +0700

    Update Toolbar.jsx

--- src/shared/components/desktop/Toolbar.jsx
+++ src/shared/components/desktop/Toolbar.jsx
@@ -1,1 +1,24 @@
+export default function Toolbar({
+  left,
+  right,
+}) {
+  return (
+    <div
+      className="
+        mb-4
+        flex
+        items-center
+        justify-between
+        gap-3
+      "
+    >
+      <div className="flex items-center gap-2">
+        {left}
+      </div>
 
+      <div className="flex items-center gap-2">
+        {right}
+      </div>
+    </div>
+  );
+}
