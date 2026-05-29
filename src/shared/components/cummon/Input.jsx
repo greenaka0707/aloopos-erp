1d0bfb34b144ab566b88460f4b5ce0069d7fec7c
greenaka0707 <produkpebeka@gmail.com>
2026-05-29 14:58:42 +0700

    Update Input.jsx

--- src/shared/components/cummon/Input.jsx
+++ src/shared/components/cummon/Input.jsx
@@ -1,1 +1,26 @@
-
+export default function Input({
+  className = "",
+  ...props
+}) {
+  return (
+    <input
+      {...props}
+      className={`
+        h-10
+        w-full
+        rounded-md
+        border
+        border-slate-300
+        bg-white
+        px-3
+        text-sm
+        outline-none
+        transition
+        focus:border-blue-500
+        focus:ring-2
+        focus:ring-blue-100
+        ${className}
+      `}
+    />
+  );
+}
