b88486224c56063d7a9726d8c4133956ed953483
greenaka0707 <produkpebeka@gmail.com>
2026-05-29 15:24:40 +0700

    Update BOMPage.jsx

--- src/modules/manufacturing/pages/BOMPage.jsx
+++ src/modules/manufacturing/pages/BOMPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 
-import Modal from "@/shared/components/Modal";
+import Modal from "@/shared/components/common/Modal";
 
 import BOMForm from "../components/BOMForm";
 
