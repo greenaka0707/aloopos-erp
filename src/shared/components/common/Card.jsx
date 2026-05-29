0a687f21de1092bd183b557f1751988471b04498
greenaka0707 <produkpebeka@gmail.com>
2026-05-29 16:49:51 +0700

    Create Card.jsx

---
+++ src/shared/components/common/Card.jsx
@@ -0,0 +1,19 @@
+export default function Card({
+  children,
+  className = "",
+}) {
+  return (
+    <div
+      className={`
+        rounded-lg
+        border
+        border-slate-200
+        bg-white
+        p-4
+        ${className}
+      `}
+    >
+      {children}
+    </div>
+  );
+}
